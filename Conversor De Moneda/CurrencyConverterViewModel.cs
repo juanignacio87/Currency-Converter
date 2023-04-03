357aac134c9438e5d614eeb07a2fc0fd1a191941
juanignacio87 <97361082+juanignacio87@users.noreply.github.com>
2023-04-02 23:07:32 -0300

    Agregar archivos de proyecto.

---
+++ Conversor De Moneda/CurrencyConverterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Conversor_De_Moneda
+{
+    internal class CurrencyConverterViewModel : Notifier 
+    {
+        private decimal pesos;
+
+        public decimal Pesos
+        {
+            get { return pesos; }
+            set
+            {
+                pesos = value;
+                OnPropertyChanged("Pesos");
+                OnPesosChanged();
+            }
+        }
+
+        private decimal dolares;
+
+        public decimal Dolares
+        {
+            get { return dolares; }
+            set
+            {
+                dolares = value;
+                OnPropertyChanged("Dolares");
+            }
+        }
+        
+        private void OnPesosChanged()
+        {
+            Dolares = Pesos / 395M;
+        }
+    }
+}
